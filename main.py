e0ecadd1837b54cbc5f7fcdf14095a5a6202a888
Wes <wes@otherdata.com>
2022-11-09 18:17:31 -0500

    set debug to info

--- main.py
+++ main.py
@@ -26,7 +26,7 @@ fhandler = logging.FileHandler(filename='/opt/mirrorsync/mirrorsync.log', mode='
 formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 fhandler.setFormatter(formatter)
 logger.addHandler(fhandler)
-logger.setLevel(logging.DEBUG)
+logger.setLevel(logging.INFO)
 
 # Build the custom mirror images on startup
 print('Setting up different container file components...')
