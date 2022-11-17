65fe7900aae1b2d9046dbafd2e78ebb79920161e
Wes <wes@otherdata.com>
2022-11-17 14:09:36 -0500

    add docker authentication in case needed

--- container_mirror/container_mirror_run.py
+++ container_mirror/container_mirror_run.py
@@ -23,27 +23,35 @@ cursor.close()
 
 # Function to inspect images
 def getimagedigest(name):
-    skopeoinspectcmd = ['inspect','docker://' + name ]
     try:
         client = docker.from_env()
-        result = client.containers.run('container-mirror:v1.0',command=skopeoinspectcmd,remove=True,user=cfg['mirrorsync']['systemduser'],network_mode=cfg['mirrorsync']['networkmode'],use_config_proxy=cfg['mirrorsync']['configproxy'])
+        # Check if docker authentication is enabled
+        if cfg['skopeo']['authentication']['enabled'] == True:
+            skopeoinspectcmd = ['inspect','--authfile=/tmp/auth.json','docker://' + name ]
+            # Run container to inspect container image and get the image digest with authentication
+            result = client.containers.run('container-mirror:v1.0',volumes={cfg['skopeo']['authentication']['authfile']: {'bind': '/tmp/auth.json', 'mode': 'ro'}},command=skopeoinspectcmd,remove=True,user=cfg['mirrorsync']['systemduser'],network_mode=cfg['mirrorsync']['networkmode'],use_config_proxy=cfg['mirrorsync']['configproxy'])
+        else:
+            skopeoinspectcmd = ['inspect','docker://' + name ]
+            # Run container to inspect container image and get the image digest without authentication
+            result = client.containers.run('container-mirror:v1.0',command=skopeoinspectcmd,remove=True,user=cfg['mirrorsync']['systemduser'],network_mode=cfg['mirrorsync']['networkmode'],use_config_proxy=cfg['mirrorsync']['configproxy'])
+        
         jsonresult = json.loads(result)
         imagedigest = jsonresult['Digest']
     except Exception as e:
         logger.error(e)
         logger.error('Could not get digest of image')
-    
     return imagedigest
 
 
 # Function to run skopeo
 def skopeosync(dockerimage):
-
-    if cfg['skopeo']['dryrun'] == True:
-        skopeocmd = ['sync','--keep-going','--dry-run','--scoped','--src','docker','--dest', 'dir', dockerimage,'/mnt/repos']
-    else:
-        skopeocmd = ['sync','--keep-going','--scoped','--src','docker','--dest','dir', dockerimage,'/mnt/repos']
-     
+    skopeocmd = ['sync','--keep-going']
+    if cfg['skopeo']['dryrun']:
+        skopeocmd.append("--dry-run")
+    if cfg['skopeo']['authentication']['enabled']:
+        skopeocmd.append('--authfile=' + cfg['skopeo']['authentication']['authfile'])
+    skopeocmd.extend(['--scoped','--src','docker','--dest', 'dir', dockerimage,'/mnt/repos'])
+            
     # Try to get image with scopeo
     try:
         imagedigest = getimagedigest(dockerimage)
@@ -52,26 +60,37 @@ def skopeosync(dockerimage):
             print('Skopeo Sync: Image already synced - ' + dockerimage + ' ' + imagedigest)
             logger.info('Skopeo Sync: Image already synced - ' + dockerimage + ' ' + imagedigest)
         else:
-            # Try syncing the image
-            print('Skopeo Sync: Syncing image ' + dockerimage + ' ' + imagedigest)
-            logger.info('Skopeo Sync: Syncing image ' + dockerimage + ' ' + imagedigest)
-            client = docker.from_env()
-            client.containers.run('container-mirror:v1.0',volumes={cfg['skopeo']['destination']: {'bind': '/mnt/repos', 'mode': 'rw'}},command=skopeocmd,remove=True,user=cfg['mirrorsync']['systemduser'],network_mode=cfg['mirrorsync']['networkmode'],use_config_proxy=cfg['mirrorsync']['configproxy'])
+            try:
+                # Try syncing the image
+                print('Skopeo Sync: Syncing image ' + dockerimage + ' ' + imagedigest)
+                logger.info('Skopeo Sync: Syncing image ' + dockerimage + ' ' + imagedigest)
+                client = docker.from_env()
+                if cfg['skopeo']['authentication']['enabled']:
+                    client.containers.run('container-mirror:v1.0',volumes={cfg['skopeo']['authentication']['authfile']: {'bind': '/tmp/auth.json', 'mode': 'ro'},cfg['skopeo']['destination']: {'bind': '/mnt/repos', 'mode': 'rw'}},command=skopeocmd,remove=True,user=cfg['mirrorsync']['systemduser'],network_mode=cfg['mirrorsync']['networkmode'],use_config_proxy=cfg['mirrorsync']['configproxy'])
+                else:
+                    client.containers.run('container-mirror:v1.0',volumes={cfg['skopeo']['destination']: {'bind': '/mnt/repos', 'mode': 'rw'}},command=skopeocmd,remove=True,user=cfg['mirrorsync']['systemduser'],network_mode=cfg['mirrorsync']['networkmode'],use_config_proxy=cfg['mirrorsync']['configproxy'])
+            except Exception as e:
+                logger.error('Skopeo Sync: There was an error with the skopeo sync')
+                logger.error(e)
+                print('Skopeo Sync: There was an error with the skopeo sync')
+                print(e)
+                return False
+            else:
+                print('Skopeo Sync: Successfully synced image ' + dockerimage + ' ' + imagedigest)
+                logger.info('Skopeo Sync: Successfully synced image ' + dockerimage + ' ' + imagedigest)
+                # Write image name, and digest to database so it is not downloaded again.
+                writedb(dockerimage, imagedigest)
 
-            # Write image name, and digest to database so it is not downloaded again.
-            writedb(dockerimage, imagedigest)
+                # Run function to syncronize container to destination
+                rsynccontainermirror()
 
-            # Run function to syncronize container to destination
-            rsynccontainermirror()
-
-            return True
-
+                return True
+            
     except Exception as e:
         logger.error('Skopeo Sync: There was an error with the skopeo sync')
         logger.error(e)
         print('Skopeo Sync: There was an error with the skopeo sync')
         print(e)
-    
 
 
 # Function to check database if item has been synced before 
@@ -99,38 +118,31 @@ def writedb(name, digest):
 def rsynccontainermirror():
 
     if cfg['skopeo']['rsync']['enabled'] == True:
-
         try:
             subprocess.call(['rsync',
             '--remove-source-files',
             '-avz',
             cfg['skopeo']['destination'],
             cfg['rsync']['sshuser'] + '@' + cfg['rsync']['sshserver'] + ':' + cfg['skopeo']['rsync']['rsyncdestination']])
-
         except Exception as e:
             logger.error(e)
             print(e)
-
         else:
             # Write a file showing completed
             finishpath = cfg['skopeo']['destination'] + '/completed.txt'
             f = open(finishpath, "x")
             f.write("completed transfer run")
 
-
             subprocess.call(['rsync',
             '--remove-source-files',
             '-avz',
             cfg['skopeo']['destination'] + '/completed.txt',
             cfg['rsync']['sshuser'] + '@' + cfg['rsync']['sshserver'] + ':' + cfg['skopeo']['rsync']['rsyncdestination']])
-
         finally:    
             # For Skopeo remove old directories, as Skopeo currently doesn't support syncing files
             subprocess.call(['find',cfg['skopeo']['destination'],'-empty','-delete'])
-
     else:
         return
-
 
 
 # Main function for running this module
@@ -156,11 +168,10 @@ def runcontainermirror():
         if ':' not in line:
             logger.info('Skopeo Sync: There is no tag. Checking for available tags...')
             print('Skopeo Sync: There is no tag. Checking for available tags...')
-            skopeolisttagscmd = ['list-tags','docker://' + line ]
-
             # Run skopeo list tags to get all tags for image
             try:
                 client = docker.from_env()
+                skopeolisttagscmd = ['list-tags','docker://' + line ]
                 result = client.containers.run('container-mirror:v1.0',command=skopeolisttagscmd,remove=True,user=cfg['mirrorsync']['systemduser'],network_mode=cfg['mirrorsync']['networkmode'],use_config_proxy=cfg['mirrorsync']['configproxy'])
                 jsonresult = json.loads(result)
 
