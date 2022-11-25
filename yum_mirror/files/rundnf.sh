10cde39de5305289fec9b3990df6e6c0c283c281
Wes <wes@otherdata.com>
2022-11-25 09:13:06 -0500

    remove generated files with rm

--- yum_mirror/files/rundnf.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-# Refresh mirror every X hours
-/bin/dnf reposync -g --delete -p /mnt/repos/ --repoid=saltstack-redhat8 --download-metadata
-/bin/dnf reposync -g --delete -p /mnt/repos/ --repoid=centos-7-base --download-metadata
